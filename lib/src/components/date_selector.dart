05eb45e9509f77bce6618c2d1d3a823e05d925f5
Sasha Moak <sasha.moak@gmail.com>
2024-10-25 22:33:19 -0700

    chore: move the DateSelector component to the components dir

---
+++ lib/src/components/date_selector.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pwhl_flutter/src/date_fns.dart';
+
+final dateProvider = StateProvider((ref) => getToday());
+
+class DateSelectorWidget extends ConsumerWidget {
+  const DateSelectorWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final date = ref.watch(dateProvider);
+    return Row(children: [
+      IconButton(
+        icon: const Icon(Icons.arrow_back),
+        tooltip: 'previous day',
+        onPressed: () {
+          ref.read(dateProvider.notifier).state =
+              date.subtract(const Duration(days: 1));
+        },
+      ),
+      const Spacer(),
+      GestureDetector(
+          onTap: () async {
+            final selectedDate = await showDatePicker(
+                context: context,
+                initialDate: date,
+                firstDate: date.subtract(const Duration(days: 365)),
+                lastDate: date.add(const Duration(days: 365)));
+            if (selectedDate != null) {
+              ref.read(dateProvider.notifier).state = selectedDate;
+            }
+          },
+          child: Text(DateFormat.yMMMd().format(date))),
+      const Spacer(),
+      IconButton(
+          icon: const Icon(Icons.arrow_forward),
+          tooltip: 'next day',
+          onPressed: () {
+            ref.read(dateProvider.notifier).state =
+                date.add(const Duration(days: 1));
+          })
+    ]);
+  }
+}
