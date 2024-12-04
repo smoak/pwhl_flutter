af2802c508342932345a640307420e06ba17f304
Sasha Moak <sasha.moak@gmail.com>
2024-12-04 09:28:01 -0800

    feat: pull to refresh and refresh after resumed
    implement pull to refresh on home view
    also refresh home view if the app is resumed and there are any live
    games

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -6,29 +6,68 @@ import 'package:pwhl_flutter/src/data/types.dart';
 import 'package:pwhl_flutter/src/provider.dart';
 import './layout.dart';
 
-class HomeView extends ConsumerWidget {
+class HomeView extends ConsumerStatefulWidget {
   const HomeView({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  HomeViewState createState() => HomeViewState();
+}
+
+class HomeViewState extends ConsumerState<HomeView> {
+  late final AppLifecycleListener _listener;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _listener = AppLifecycleListener(
+      onStateChange: _onStateChanged,
+    );
+  }
+
+  @override
+  void dispose() {
+    _listener.dispose();
+
+    super.dispose();
+  }
+
+  void _onStateChanged(AppLifecycleState state) {
+    final date = ref.read(dateProvider);
+    final shouldRefresh = ref
+            .read(scheduleProvider((date: date)))
+            .valueOrNull
+            ?.any((g) => g.gameState == GameState.live) ??
+        false;
+
+    if (state == AppLifecycleState.resumed && shouldRefresh) {
+      ref.invalidate(scheduleProvider((date: date)));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final date = ref.watch(dateProvider);
     final AsyncValue<List<Game>> games =
         ref.watch(scheduleProvider((date: date)));
 
     return Layout(
         title: "Schedule",
-        child: Center(
-            child: Column(children: [
-          Container(
-              padding: const EdgeInsets.only(top: 8),
-              child: const DateSelectorWidget()),
-          switch (games) {
-            AsyncData(:final value) => Expanded(child: GameList(items: value)),
-            AsyncError() => const Text(
-                'Oops, something unexpected happened',
-              ),
-            _ => const CircularProgressIndicator(),
-          }
-        ])));
+        child: RefreshIndicator(
+            onRefresh: () => ref.refresh(scheduleProvider((date: date)).future),
+            child: Center(
+                child: Column(children: [
+              Container(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: const DateSelectorWidget()),
+              switch (games) {
+                AsyncData(:final value) =>
+                  Expanded(child: GameList(items: value)),
+                AsyncError() => const Text(
+                    'Oops, something unexpected happened',
+                  ),
+                _ => const CircularProgressIndicator(),
+              }
+            ]))));
   }
 }
